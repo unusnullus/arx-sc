6a574e2097eea55734b16d72bee47f68c69046f3
pepavlad <pepelvlad@gmail.com>
2025-11-20 12:49:34 +0100

    minor UI changes

--- apps/web/src/app/buy/page.tsx
+++ apps/web/src/app/buy/page.tsx
@@ -4,8 +4,8 @@ import { RecentTransaction } from "@/widgets/recent-transactions";
 
 export default function BuyPage() {
   return (
-    <main className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      <PoolTransactions className="max-w-full" />
+    <main className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2 md:px-0">
+      <PoolTransactions className="md:max-w-full" />
       <TokenInfo />
       <RecentTransaction />
     </main>
